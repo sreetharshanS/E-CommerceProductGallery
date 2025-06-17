0f6214ee4b44713cb7a8c00ef26fb42526464792
Sree Tharshan S <sreetharshans@gmail.com>
2025-06-17 12:43:03 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,77 @@
+ //card1
+ document.getElementById('hero-img1').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img1').src ="Images/card1img2.webp";
+       document.getElementById('hero-img1').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img1').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img1').src = "Images/card1img.webp";
+ });
+ //card2
+  document.getElementById('hero-img2').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img2').src ="Images/card2img2.webp";
+       document.getElementById('hero-img2').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img2').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img2').src = "Images/cardimg2.webp";
+ });
+
+ //card3
+  document.getElementById('hero-img3').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img3').src ="Images/card3img.webp";
+       document.getElementById('hero-img3').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img3').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img3').src = "Images/card3img2.webp";
+ });
+
+ //card4
+   document.getElementById('hero-img4').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img4').src ="Images/card4img2.webp";
+       document.getElementById('hero-img4').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img4').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img4').src = "Images/card4img.webp";
+ });
+ //card5
+   document.getElementById('hero-img5').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img5').src ="Images/card5img2.webp";
+       document.getElementById('hero-img5').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img5').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img5').src = "Images/card5img.webp";
+ });
+
+ //card6
+   document.getElementById('hero-img6').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img6').src ="Images/card6img2.webp";
+       document.getElementById('hero-img6').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img6').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img6').src = "Images/card6img.webp";
+ });
+
+ //card7
+   document.getElementById('hero-img7').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img7').src ="Images/card1img2.webp";
+       document.getElementById('hero-img7').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img7').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img7').src = "Images/card1img.webp";
+ });
+
+  //card8
+   document.getElementById('hero-img8').addEventListener('mouseover',()=>{
+       document.getElementById('hero-img8').src ="Images/card2img2.webp";
+       document.getElementById('hero-img8').style.transition ="opacity 10s  ease-in-out; "
+ });
+
+ document.getElementById('hero-img8').addEventListener('mouseout',()=>{
+       document.getElementById('hero-img8').src = "Images/card3img.webp";
+ });
